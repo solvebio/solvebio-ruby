3627d28b9d81f08de56c05975d7a14e67724f16e
rocky <rb@dustyfeet.com>
2014-09-01 09:37:28 -0400

    Last of demos from https://www.solvebio.com/docs/api

--- demo/dataset/field.rb
+++ demo/dataset/field.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-# Simple use of SolveBio::DatsetField.retrieve ... facets
+# Simple use of SolveBio::DatsetField.retrieve
 
 require 'solvebio'
 
 # SolveBio::Client.client.api_key = 'set-me-correctly'
 if SolveBio::Client.client.api_key
-    fields = SolveBio::DatasetField.retrieve(691).facets
+    fields = SolveBio::DatasetField.retrieve(1)
     puts fields
 else
     puts 'Please set SolveBio::Client.client.api_key. Hint: solvebio.rb login'
