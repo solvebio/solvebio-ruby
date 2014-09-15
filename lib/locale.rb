37806372de05a2694aa95e98f880e560b1c9f8f8
rocky <rb@dustyfeet.com>
2014-09-14 22:14:43 -0400

    Forget to add locale in previous commit

---
+++ lib/locale.rb
@@ -0,0 +1,38 @@
+require_relative 'main'
+module SolveBio::Locale
+
+    # Used only if r18n-core is not around
+    @thousands_sep  = ','
+    def thousands_sep
+        @thousands_sep
+    end
+    def thousands_sep=(value)
+        @thousands_sep = value
+    end
+
+    if require 'r18n-core'
+        @locale = ENV['LANG'] || ENV['LC_NUMERIC'] || 'en_US.UTF-8'
+        R18n.set(@locale)
+        def pretty_int(num)
+            R18n::l(num)
+        end
+    else
+        def pretty_int(num)
+            num.to_s.reverse.scan(/\d{1,3}/).join(@thousands_sep).reverse
+        end
+    end
+
+    module_function :pretty_int
+
+end
+
+class Fixnum
+    include SolveBio::Locale
+    def pretty_int
+        SolveBio::Locale.pretty_int(self)
+    end
+end
+
+if __FILE__ == $0
+    puts 10000.pretty_int
+end
