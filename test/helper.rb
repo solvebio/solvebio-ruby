41e7bd5387f23acd78b546163592aef56da9c6dc
rocky <rocky@solvebio.com>
2014-10-29 09:54:17 -0400

    Set up environment better for Travis testing.

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,7 @@
 require 'test/unit'
 require_relative '../lib/main'
+require_relative '../lib/solvebio'
+ENV['SOLVEBIO_API_HOST'] ||= 'http://api.solvebio.com'
 
 def local_api?
     ENV['SOLVEBIO_API_HOST'].start_with?('http://127.0.0.1')
