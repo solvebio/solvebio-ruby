0feceb4bdef011bf7f074ea16bc5578f2e0d6088
rocky <rocky@solvebio.com>
2014-11-03 13:59:52 -0500

    Change defaulit dataset from ClinVar/2.0.0-1/Variants to HGNC/1.0.0-1/HGNC except where we need a 2nd dataset.

--- test/helper.rb
+++ test/helper.rb
@@ -2,6 +2,9 @@ require 'test/unit'
 ENV['SOLVEBIO_API_HOST'] ||= 'https://api.solvebio.com'
 require_relative '../lib/main'
 
+TEST_DATASET_NAME = 'HGNC/1.0.0-1/HGNC'
+
+
 def local_api?
     ENV['SOLVEBIO_LOCAL_API']
 end
